ff7e43998fed6781717f6d219220207e0b9548f5
Shaun Parkison <shparkison@gmail.com>
2024-12-21 10:25:57 -0700

    Add php info page; work on getting Swoole working

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 // Generate M3U playlist from the playlist configuration
 Route::get('/playlists/{uuid}/generate', PlaylistGenerateController::class)
     ->name('playlists.generate');
+
+// If local env, show PHP info screen
+Route::get('/phpinfo', function () {
+    if (app()->environment('local')) {
+        phpinfo();
+    } else {
+        abort(404);
+    }
+});
