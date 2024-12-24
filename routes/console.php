52bd69e7db8956c1b4650998708cd0fe22b9d04a
Shaun Parkison <shparkison@gmail.com>
2024-12-24 10:25:51 -0700

    Update broadcasting settings

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\User;
-use Filament\Notifications\Notification;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -12,17 +10,3 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('app:refresh-playlist')
     ->everyFiveMinutes();
 
-Schedule::call(function () {
-    $user = User::first();
-    $now = now();
-    Notification::make()
-        ->success()
-        ->title('Notification broadcast')
-        ->body("Notification broadcast on: \"{$now->toDateTimeString()}\"")
-        ->broadcast($user);
-    Notification::make()
-        ->success()
-        ->title('Notification database')
-        ->body("Notification database on: \"{$now->toDateTimeString()}\"")
-        ->sendToDatabase($user);
-})->everyMinute();
