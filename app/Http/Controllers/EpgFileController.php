262d01e208ed0245d55e0a49fed693bcc35bbb87
Shaun Parkison <shparkison@gmail.com>
2025-02-01 15:03:09 -0700

    Update EpgFileController.php

--- app/Http/Controllers/EpgFileController.php
+++ app/Http/Controllers/EpgFileController.php
@@ -6,6 +6,7 @@ use App\Models\Epg;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class EpgFileController extends Controller
 {
@@ -15,35 +16,18 @@ class EpgFileController extends Controller
         if (!Storage::exists($epg->file_path)) {
             abort(404);
         }
-        $filePath = Storage::disk('local')->path($epg->file_path);
-        $fileStream = Storage::disk('local')->readStream($epg->file_path);
-        $fileSize = filesize($filePath);
-        $headers = [
-            'Content-type' => 'application/xml',
-            'Content-Disposition' => 'attachment; filename=epg.xml', // if we need to force download
-            'Pragma' => 'no-cache',
-            'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
-            'Expires' => '0',
-            'Content-Length' => $fileSize,
-        ];
-        set_time_limit(0);
-        $end = $fileSize - 1;
-        $callback = function () use ($fileStream, $end) {
-            ob_get_clean();
-            $i = 0;
-            $buffer = 102400;
-            while (!feof($fileStream) && $i <= $end) {
-                $bytesToRead = $buffer;
-                if ($i + $bytesToRead > $end) {
-                    $bytesToRead = $end - $i + 1;
-                }
-                $data = fread($fileStream, $bytesToRead);
-                echo $data;
-                flush();
-                $i += $bytesToRead;
-            }
-            fclose($fileStream);
-        };
-        return Response::stream($callback, 200, $headers)->send();
+        $fs = Storage::getDriver();
+        $stream = $fs->readStream($epg->file_path);
+        return response()->stream(
+            function () use ($stream) {
+                while (ob_get_level() > 0) ob_end_flush();
+                fpassthru($stream);
+            },
+            200,
+            [
+                'Content-Type' => 'application/xml',
+                'Content-disposition' => 'attachment; filename="' . Str::slug($epg->name) . '.xml"',
+            ]
+        );
     }
 }
