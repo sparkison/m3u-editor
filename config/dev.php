5b7c0d0b368932374c6bb7e50f3f14ec78d531ca
Shaun Parkison <shparkison@gmail.com>
2025-02-22 15:44:27 -0700

    Version bump for next release

--- config/dev.php
+++ config/dev.php
@@ -2,7 +2,7 @@
 
 return [
     'author' => 'Shaun Parkison',
-    'version' => '0.2.12',
+    'version' => '0.2.13',
     'repo' => 'sparkison/m3u-editor',
     'donate' => 'https://buymeacoffee.com/shparkison',
 ];
