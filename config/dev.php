d563975a6facb49ced5f0adf1edbd6636fa9392e
Shaun Parkison <shparkison@gmail.com>
2025-03-10 14:03:27 -0600

    Fix parsing error with non-standard M3U lines

--- config/dev.php
+++ config/dev.php
@@ -2,7 +2,7 @@
 
 return [
     'author' => 'Shaun Parkison',
-    'version' => '0.2.25',
+    'version' => '0.2.26',
     'repo' => 'sparkison/m3u-editor',
     'donate' => 'https://buymeacoffee.com/shparkison',
     'discord_url' => 'https://discord.gg/rS3abJ5dz7',
