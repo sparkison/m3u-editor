1124a32af368f431cd67445eeeef61e6fa4135a8
Shaun Parkison <shparkison@gmail.com>
2025-03-10 16:27:42 -0600

    Update log drive to daily and cap to 3 days worth of logs

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => 'daily', // env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,7 +69,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => env('LOG_DAILY_DAYS', 14),
+            'days' => env('LOG_DAILY_DAYS', 3),
             'replace_placeholders' => true,
         ],
 
