aae110155c2cf03712f3b965bc457406dbe084a7
Shaun Parkison <shparkison@gmail.com>
2025-03-11 17:27:17 -0600

    Update backup settings and start container to remove old log file

--- config/backup.php
+++ config/backup.php
@@ -14,11 +14,14 @@ return [
                 /*
                  * The list of directories and files that will be included in the backup.
                  */
-                'include' => array_merge(
-                    [],
-                    glob(base_path('storage/app/private/epg/*')),
-                    glob(base_path('storage/app/private/playlist/*'))
-                ),
+//                'include' => array_merge(
+//                    [],
+//                    glob(base_path('storage/app/private/epg/*')),
+//                    glob(base_path('storage/app/private/playlist/*'))
+//                ),
+                'include' => [
+                    // ...
+                ],
 
                 /*
                  * These directories and files will be excluded from the backup.
