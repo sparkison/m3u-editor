954c1333ab1372d2b0385c7a7a9f1f01f71f8bf2
Shaun Parkison <shparkison@gmail.com>
2025-03-17 14:32:55 -0600

    Update dev.php

--- config/dev.php
+++ config/dev.php
@@ -2,7 +2,7 @@
 
 return [
     'author' => 'Shaun Parkison',
-    'version' => '0.3.0',
+    'version' => '0.3.1',
     'repo' => 'sparkison/m3u-editor',
     'donate' => 'https://buymeacoffee.com/shparkison',
     'discord_url' => 'https://discord.gg/rS3abJ5dz7',
