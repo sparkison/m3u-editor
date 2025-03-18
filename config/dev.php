da3bc2cd43b39f035b1a155c65e433215645c2c9
Shaun Parkison <shparkison@gmail.com>
2025-03-18 13:14:01 -0600

    Fix migration error

--- config/dev.php
+++ config/dev.php
@@ -2,7 +2,7 @@
 
 return [
     'author' => 'Shaun Parkison',
-    'version' => '0.3.5',
+    'version' => '0.3.5.1',
     'repo' => 'sparkison/m3u-editor',
     'donate' => 'https://buymeacoffee.com/shparkison',
     'discord_url' => 'https://discord.gg/rS3abJ5dz7',
--- database/migrations/2025_03_18_171618_update_sort_column_on_channels.php
+++ database/migrations/2025_03_18_171618_update_sort_column_on_channels.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        Schema::dropIfExists('__temp__channels');
         Schema::table('channels', function (Blueprint $table) {
-            $table->decimal('sort', 12, 4)->default(0)->change();
+            $table->decimal('sort', 12, 4)
+                ->nullable()
+                ->change();
         });
     }
 
