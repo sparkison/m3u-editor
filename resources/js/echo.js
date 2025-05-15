fd295ee98f61e0f5a72bd88c48f9d979c695570e
Shaun Parkison <shparkison@gmail.com>
2025-05-15 07:55:08 -0600

    Disable websocket client if broadcasting disabled

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,21 +1,25 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
-window.Pusher = Pusher;
-
-const env = import.meta.env.VITE_REVERB_ENV ?? 'production';
-const scheme = import.meta.env.VITE_REVERB_SCHEME ?? 'https'
-let wsPort = import.meta.env.VITE_REVERB_PORT;
-let wssPort = scheme === 'https' ? 443 : wsPort;
-if (env !== 'production') {
-    wssPort = wsPort
+const disabled = import.meta.env.VITE_WEBSOCKETS_DISABLED ?? 'false';
+if (disabled === 'true') {
+    window.Echo = null;
+} else {
+    window.Pusher = Pusher;
+    const env = import.meta.env.VITE_REVERB_ENV ?? 'production';
+    const scheme = import.meta.env.VITE_REVERB_SCHEME ?? 'https'
+    let wsPort = import.meta.env.VITE_REVERB_PORT;
+    let wssPort = scheme === 'https' ? 443 : wsPort;
+    if (env !== 'production') {
+        wssPort = wsPort
+    }
+    window.Echo = new Echo({
+        broadcaster: 'reverb',
+        key: import.meta.env.VITE_REVERB_APP_KEY,
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort,
+        wssPort,
+        forceTLS: scheme === 'https',
+        enabledTransports: ['ws', 'wss'],
+    });
 }
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort,
-    wssPort,
-    forceTLS: scheme === 'https',
-    enabledTransports: ['ws', 'wss'],
-});
+
