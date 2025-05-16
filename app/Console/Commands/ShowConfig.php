7f1bb5f87d36c1bd757ccb13f662771dcb61a0b3
Shaun Parkison <shparkison@gmail.com>
2025-05-16 10:48:51 -0600

    Remove debug statement

--- app/Console/Commands/ShowConfig.php
+++ app/Console/Commands/ShowConfig.php
@@ -7,23 +7,19 @@ use Illuminate\Console\Command;
 class ShowConfig extends Command
 {
     protected $signature = 'config:show {key? : Only get this one key}';
-
     protected $description = 'Shows the config';
 
     public function handle()
     {
         if ($specifiedKey = $this->argument('key')) {
-        $loop = [$specifiedKey => config($specifiedKey)];
+            $loop = [$specifiedKey => config($specifiedKey)];
         } else {
-        $loop = config()->all();
+            $loop = config()->all();
         }
-
         foreach ($loop as $key => $config) {
-        $this->info($key);
-        dump($config);
-        $this->newLine();
+            $this->info($key);
+            $this->newLine();
         }
-
         return Command::SUCCESS;
     }
 }
