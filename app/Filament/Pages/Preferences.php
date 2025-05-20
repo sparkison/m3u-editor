7a2943dc4932bcdecb84d968ba0831d2651f9cea
Shaun Parkison <shparkison@gmail.com>
2025-05-20 14:15:21 -0600

    Update Preferences.php

--- app/Filament/Pages/Preferences.php
+++ app/Filament/Pages/Preferences.php
@@ -109,7 +109,7 @@ class Preferences extends SettingsPage
                                             ->columnSpan(2)
                                             ->default('VLC/3.0.21 LibVLC/3.0.21')
                                             ->placeholder('VLC/3.0.21 LibVLC/3.0.21')
-                                            ->helperText(''),
+                                            ->helperText('Fallback user agent (defaults to Playlist user agent when available).'),
                                         $this->makeCodecSelect('video', 'ffmpeg_codec_video', 'videoCodecs'),
                                         $this->makeCodecSelect('audio', 'ffmpeg_codec_audio', 'audioCodecs'),
                                         $this->makeCodecSelect('subtitle', 'ffmpeg_codec_subtitles', 'subtitleCodecs'),
