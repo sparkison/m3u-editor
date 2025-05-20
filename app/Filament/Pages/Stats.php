a1536d5b72697c9500728700a96a7f04fac94df1
Shaun Parkison <shparkison@gmail.com>
2025-05-20 14:10:16 -0600

    Hide stats page for now

--- app/Filament/Pages/Stats.php
+++ app/Filament/Pages/Stats.php
@@ -12,10 +12,12 @@ class Stats extends Page
     protected static ?string $navigationIcon  = 'heroicon-o-chart-bar';
     protected static ?string $navigationLabel = 'Stats';
     protected static ?string $title           = 'Stats';
-    protected ?string        $subheading      = 'Start streaming a channel to see the stats.';
+    //protected ?string        $subheading      = 'Start streaming a channel to see the stats.';
+    protected ?string        $subheading      = 'Stats are currently disabled until more testing can be done.';
     protected static ?string $navigationGroup = 'Tools';
     protected static ?int    $navigationSort  = 99;
     protected static string  $view            = 'filament.pages.stats';
+    protected static bool    $shouldRegisterNavigation = false; // hide for now, needs more testing...
 
     public static ?string $pollingInterval = '5s';
 
