597cb8b9674f9d1744fbf14674e41b74810da4ab
Shaun Parkison <shparkison@gmail.com>
2025-06-30 15:03:35 -0600

    feat: Update channel EPG mapper to exclude VOD channels
    Only applies to Xtream API imported playlists

--- app/Jobs/MapPlaylistChannelsToEpg.php
+++ app/Jobs/MapPlaylistChannelsToEpg.php
@@ -98,20 +98,24 @@ class MapPlaylistChannelsToEpg implements ShouldQueue
             // Fetch the playlist (if set)
             $channels = [];
             if ($this->channels) {
-                $totalChannelCount = count($this->channels);
-                $mappedCount = collect($this->channels)
+                $channels = collect($this->channels);
+                $totalChannelCount = $channels->where('is_vod', false)->count();
+                $mappedCount = $channels
                     ->filter(fn($channel) => $channel->epg_channel_id !== null)
                     ->count();
-                $channels = Channel::whereIn('id', $this->channels)
+                $channels = Channel::whereIn('id', $channels->pluck('id'))
+                    ->where('is_vod', false)
                     ->when(!$this->force, function ($query) {
                         $query->where('epg_channel_id', null);
                     })->cursor();
             } else if ($playlist) {
-                $totalChannelCount = $playlist->channels()->count();
+                $totalChannelCount = $playlist->channels()->where('is_vod', false)->count();
                 $mappedCount = $playlist->channels()
+                    ->where('is_vod', false)
                     ->whereNotNull('epg_channel_id')
                     ->count();
                 $channels = $playlist->channels()
+                    ->where('is_vod', false)
                     ->when(!$this->force, function ($query) {
                         $query->where('epg_channel_id', null);
                     })->cursor();
