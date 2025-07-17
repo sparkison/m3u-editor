d08213491698f25811d77958598a0dedea439a0e
Shaun Parkison <shparkison@gmail.com>
2025-07-17 07:09:51 -0600

    chore: Remove debug lines

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,6 @@ use App\Http\Controllers\PlaylistGenerateController;
 use App\Http\Controllers\XtreamApiController;
 use Illuminate\Support\Facades\Route;
 
-use App\Jobs\MergeChannels;
-use App\Models\Channel;
-use App\Models\User;
-use Illuminate\Support\Collection;
-
-Route::get('/test-merge', function () {
-    // Create a user
-    $user = User::factory()->create();
-
-    // Create channels
-    $channel1 = Channel::factory()->create(['stream_id' => 'stream1', 'user_id' => $user->id]);
-    $channel2 = Channel::factory()->create(['stream_id' => 'stream1', 'user_id' => $user->id]);
-    $channel3 = Channel::factory()->create(['stream_id' => '', 'user_id' => $user->id]);
-    $channel4 = Channel::factory()->create(['stream_id' => null, 'user_id' => $user->id]);
-
-    $channels = new Collection([$channel1, $channel2, $channel3, $channel4]);
-
-    // Dispatch the job
-    MergeChannels::dispatch($channels, $user);
-
-    return 'Merge job dispatched';
-});
-
 /*
  * Playlist/EPG output routes
  */
