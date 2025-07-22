e2ce53f655dd462458aa1184e8502b7ae6ca4e93
Shaun Parkison <shparkison@gmail.com>
2025-07-22 07:27:21 -0600

    feat: Add assigned auth output to Xtream API section
    Fixes #274

--- resources/views/forms/components/xtream-api-info.blade.php
+++ resources/views/forms/components/xtream-api-info.blade.php
@@ -4,6 +4,7 @@
     @php($url = $info['url'])
     @php($username = $info['username'])
     @php($password = $info['password'])
+    @php($auths = $record->playlistAuths)
     <div x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }">
         <div class="">
             <p class="text-sm text-gray-500 dark:text-gray-400 mb-2">
@@ -56,10 +57,44 @@
             <p class="text-sm text-gray-500 dark:text-gray-400 mb-2">
                 The default username is your <strong>m3u editor</strong> username and the Playlist <strong>unique identifier</strong> is the password.
             </p>
-            @if($record->playlistAuths()->count() > 0)
+            @if($auths->isNotEmpty())
                 <p class="text-sm text-gray-500 dark:text-gray-400 mb-2">
                     You can also use your assigned <strong>Playlist Auths</strong> to access the Xtream API.
                 </p>
+                @foreach($auths as $auth)
+                    <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">
+                        {{ $auth->name }}
+                    </span>
+                    <div class="flex gap-2 items-center justify-start mb-4">
+                        <x-filament::input.wrapper suffix-icon="heroicon-m-user">
+                            <x-slot name="prefix">
+                                <x-copy-to-clipboard :text="$auth->username" />
+                            </x-slot> 
+                            <x-filament::input
+                                type="text"
+                                :value="$auth->username"
+                                readonly
+                            />
+                        </x-filament::input.wrapper>
+                        <x-qr-modal :title="$record->name" body="Xtream API Username" :text="$auth->username" />
+                    </div>
+                    <div class="flex gap-2 items-center justify-start mb-4">
+                        <x-filament::input.wrapper suffix-icon="heroicon-m-lock-closed">
+                            <x-slot name="prefix">
+                                <x-copy-to-clipboard :text="$auth->password" />
+                            </x-slot> 
+                            <x-filament::input
+                                type="text"
+                                :value="$auth->password"
+                                :placeholder="$auth->password"
+                                readonly
+                            />
+                        </x-filament::input.wrapper>
+                        @if($auth->password !== 'YOUR_M3U_EDITOR_PASSWORD')
+                            <x-qr-modal :title="$record->name" body="Xtream API Password" :text="$auth->password" />
+                        @endif  
+                    </div>
+                @endforeach
             @endif
         </div>
     </div>
