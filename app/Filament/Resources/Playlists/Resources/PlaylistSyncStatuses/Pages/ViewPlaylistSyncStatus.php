cc7cf5d8f7f7335b8abf078ad866dbad6650fcc6
Shaun Parkison <shparkison@gmail.com>
2025-08-23 07:38:26 -0600

    chore: Cleanup sync status view

--- app/Filament/Resources/Playlists/Resources/PlaylistSyncStatuses/Pages/ViewPlaylistSyncStatus.php
+++ app/Filament/Resources/Playlists/Resources/PlaylistSyncStatuses/Pages/ViewPlaylistSyncStatus.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Playlists\Resources\PlaylistSyncStatuses\Pages;
 
 use App\Filament\Resources\Playlists\Resources\PlaylistSyncStatuses\PlaylistSyncStatusResource;
+use App\Models\Playlist;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -10,10 +11,22 @@ class ViewPlaylistSyncStatus extends ViewRecord
 {
     protected static string $resource = PlaylistSyncStatusResource::class;
 
+    public function getParent(): Playlist
+    {
+        // For nested resources, the parent ID is available in the route parameters
+        return Playlist::findOrFail(request()->route('playlist'));
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            //
+            Actions\Action::make('back_to_sync_statuses')
+                ->label('Back to Sync Statuses')
+                ->icon('heroicon-o-arrow-left')
+                ->color('gray')
+                ->url(function (): string {
+                    return "/playlists/{$this->getParent()->id}/playlist-sync-statuses";
+                }),
         ];
     }
 }
