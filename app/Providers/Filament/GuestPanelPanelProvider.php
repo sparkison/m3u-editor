c7ec306f17bfa1353bedf32a9d792e2eed646d3f
Shaun Parkison <shparkison@gmail.com>
2025-08-29 13:22:48 -0600

    chore: Remove series page, we'll use a resource instead

--- app/Providers/Filament/GuestPanelPanelProvider.php
+++ app/Providers/Filament/GuestPanelPanelProvider.php
@@ -30,8 +30,8 @@ class GuestPanelPanelProvider extends PanelProvider
     {
         return $panel
             ->id('playlist')
-            ->path('playlist/{uuid}')
             ->userMenu(false)
+            ->path('playlist/{uuid}')
             ->homeUrl(function () {
                 $uuid = request()->route('uuid') ?? request()->attributes->get('playlist_uuid');
                 return $uuid ? route('filament.playlist.home', ['uuid' => $uuid]) : '/';
