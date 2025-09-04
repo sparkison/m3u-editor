28b46291fd362af1d9f1d96064f32e5e4be3b29e
Shaun Parkison <shparkison@gmail.com>
2025-09-04 06:13:27 -0600

    fix: Make sure only syncing stream files for enabled episodes

--- app/Jobs/SyncSeriesStrmFiles.php
+++ app/Jobs/SyncSeriesStrmFiles.php
@@ -55,7 +55,7 @@ class SyncSeriesStrmFiles implements ShouldQueue
             }
 
             // Get the series episodes
-            $episodes = $series->episodes;
+            $episodes = $series->enabled_episodes;
 
             // Check if there are any episodes
             if ($episodes->isEmpty()) {
