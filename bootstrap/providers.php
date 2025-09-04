690d9ed542d9a8a230ac3fdd8355653c5bad2da9
Shaun Parkison <shparkison@gmail.com>
2025-09-04 09:54:33 -0600

    chore: Prep proxy for logos

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    App\Providers\LogoServiceProvider::class,
     App\Providers\VersionServiceProvider::class,
 ];
