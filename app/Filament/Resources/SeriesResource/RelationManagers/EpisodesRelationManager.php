cde88ebc7f95547859e4a29adb6ffcb7361aee4e
Shaun Parkison <shparkison@gmail.com>
2025-09-04 06:21:49 -0600

    chore: Add bulk action to Series edit page to enable/disable all episodes

--- app/Filament/Resources/SeriesResource/RelationManagers/EpisodesRelationManager.php
+++ app/Filament/Resources/SeriesResource/RelationManagers/EpisodesRelationManager.php
@@ -19,6 +19,8 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 class EpisodesRelationManager extends RelationManager
 {
     protected static string $relationship = 'episodes';
+    
+    protected $listeners = ['refreshRelation' => '$refresh'];
 
     public function isReadOnly(): bool
     {
