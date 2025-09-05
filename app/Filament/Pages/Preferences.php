82f9b3985d40fbabca33785f5fd4661b0b9cd594
Shaun Parkison <shparkison@gmail.com>
2025-09-05 09:07:20 -0600

    feat: Implement automated backups
    Resolves #320

--- app/Filament/Pages/Preferences.php
+++ app/Filament/Pages/Preferences.php
@@ -403,30 +403,39 @@ class Preferences extends SettingsPage
                                     ])
                             ]),
 
-                        Forms\Components\Tabs\Tab::make('API')
+                        Forms\Components\Tabs\Tab::make('Backups')
                             ->schema([
-                                Forms\Components\Section::make('API Settings')
+                                Forms\Components\Section::make('Automated backups')
                                     ->headerActions([
-                                        Forms\Components\Actions\Action::make('manage_api_keys')
-                                            ->label('Manage API Tokens')
-                                            ->color('gray')
-                                            ->icon('heroicon-s-key')
-                                            ->iconPosition('before')
-                                            ->size('sm')
-                                            ->url('/profile'),
-                                        Forms\Components\Actions\Action::make('view_api_docs')
-                                            ->label('API Docs')
+                                        Forms\Components\Actions\Action::make('view_cron_example')
+                                            ->label('CRON Example')
                                             ->icon('heroicon-o-arrow-top-right-on-square')
                                             ->iconPosition('after')
                                             ->size('sm')
-                                            ->url('/docs/api')
+                                            ->url('https://crontab.guru')
                                             ->openUrlInNewTab(true),
                                     ])->schema([
-                                        Forms\Components\Toggle::make('show_api_docs')
-                                            ->label('Allow access to API docs')
-                                            ->helperText('When enabled you can access the API documentation using the "API Docs" button. When disabled, the docs endpoint will return a 403 (Unauthorized). NOTE: The API will respond regardless of this setting. You do not need to enable it to use the API.'),
+                                        Forms\Components\Toggle::make('auto_backup_database')
+                                            ->label('Enable Automatic Database Backups')
+                                            ->live()
+                                            ->helperText('When enabled, automatic database backups will be created based on the specified schedule.'),
+                                        Forms\Components\Group::make()
+                                            ->columnSpanFull()
+                                            ->columns(2)
+                                            ->schema([
+                                                Forms\Components\TextInput::make('auto_backup_database_schedule')
+                                                    ->label('Backup Schedule')
+                                                    ->suffix(config('app.timezone'))
+                                                    ->helperText('Specify the CRON schedule for automatic backups, e.g. "* 3 * * *".'),
+                                                Forms\Components\TextInput::make('auto_backup_database_max_backups')
+                                                    ->label('Max Backups')
+                                                    ->type('number')
+                                                    ->minValue(0)
+                                                    ->helperText('Specify the maximum number of backups to keep. Enter 0 for no limit.'),
+                                            ])->hidden(fn($get) => !$get('auto_backup_database'))
                                     ])
                             ]),
+
                         Forms\Components\Tabs\Tab::make('SMTP')
                             ->columns(2)
                             ->schema([
@@ -532,6 +541,30 @@ class Preferences extends SettingsPage
                                             ->email()
                                             ->helperText('The "From" email address for outgoing emails. Defaults to no-reply@m3u-editor.dev.'),
                                     ]),
+                            ]),
+                        Forms\Components\Tabs\Tab::make('API')
+                            ->schema([
+                                Forms\Components\Section::make('API Settings')
+                                    ->headerActions([
+                                        Forms\Components\Actions\Action::make('manage_api_keys')
+                                            ->label('Manage API Tokens')
+                                            ->color('gray')
+                                            ->icon('heroicon-s-key')
+                                            ->iconPosition('before')
+                                            ->size('sm')
+                                            ->url('/profile'),
+                                        Forms\Components\Actions\Action::make('view_api_docs')
+                                            ->label('API Docs')
+                                            ->icon('heroicon-o-arrow-top-right-on-square')
+                                            ->iconPosition('after')
+                                            ->size('sm')
+                                            ->url('/docs/api')
+                                            ->openUrlInNewTab(true),
+                                    ])->schema([
+                                        Forms\Components\Toggle::make('show_api_docs')
+                                            ->label('Allow access to API docs')
+                                            ->helperText('When enabled you can access the API documentation using the "API Docs" button. When disabled, the docs endpoint will return a 403 (Unauthorized). NOTE: The API will respond regardless of this setting. You do not need to enable it to use the API.'),
+                                    ])
                             ]),
                         Forms\Components\Tabs\Tab::make('Debugging')
                             ->schema([
