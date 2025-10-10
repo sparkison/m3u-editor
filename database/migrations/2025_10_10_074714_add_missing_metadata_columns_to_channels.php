fa7824014a7b12b9c557139d108d46fcb71fde80
Shaun Parkison <shparkison@gmail.com>
2025-10-10 07:49:05 -0600

    fix: Add missing columns to channels
    Resolves #404

---
+++ database/migrations/2025_10_10_074714_add_missing_metadata_columns_to_channels.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->jsonb('info')->nullable()->after('logo_internal');
+            $table->jsonb('movie_data')->nullable()->after('info');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->dropColumn('info');
+            $table->dropColumn('movie_data');
+        });
+    }
+};
