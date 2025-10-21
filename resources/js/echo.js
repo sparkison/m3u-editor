9502380b4bad10a76dc3a41f11b63d0cf17bf2c4
Shaun Parkison <shparkison@gmail.com>
2025-10-21 11:14:51 -0600

    chore: Cleanup echo creation
    No need to check variable which doesn't exist

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,27 +1,20 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
-const disabled = import.meta.env.VITE_WEBSOCKETS_DISABLED ?? 'false';
+window.Pusher = Pusher;
 
-if (disabled === 'true') {
-    window.Echo = null;
-} else {
-    window.Pusher = Pusher;
-    
-    const host = window.location.hostname;
-    const scheme = window.location.protocol.slice(0, -1); // remove the trailing ':'
-    const port = window.location.port;
+const host = window.location.hostname;
+const scheme = window.location.protocol.slice(0, -1); // remove the trailing ':'
+const port = window.location.port;
 
-    const echoConfig = {
-        broadcaster: 'reverb',
-        key: "5e2a227aacd3bc04713e595428195896617947b8f5ec11db31029abd13b13538",
-        wsHost: host,
-        wsPort: port,
-        wssPort: port,
-        forceTLS: scheme === 'https',
-        enabledTransports: ['ws', 'wss'],
-    };
+const echoConfig = {
+    broadcaster: 'reverb',
+    key: "5e2a227aacd3bc04713e595428195896617947b8f5ec11db31029abd13b13538",
+    wsHost: host,
+    wsPort: port,
+    wssPort: port,
+    forceTLS: scheme === 'https',
+    enabledTransports: ['ws', 'wss'],
+};
 
-    window.Echo = new Echo(echoConfig);
-}
-
+window.Echo = new Echo(echoConfig);
