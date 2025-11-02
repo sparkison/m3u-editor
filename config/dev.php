3d7cce300b302b1adb6bc00f57b037f79831d691
Shaun Parkison <shparkison@gmail.com>
2025-11-02 09:45:17 -0700

    chore: Version bump

--- config/dev.php
+++ config/dev.php
@@ -3,8 +3,8 @@
 return [
     'author' => 'Shaun Parkison',
     'version' => '0.7.11',
-    'dev_version' => '0.8.2-dev',
-    'experimental_version' => '0.8.2-exp',
+    'dev_version' => '0.8.3-dev',
+    'experimental_version' => '0.8.3-exp',
     'repo' => 'sparkison/m3u-editor',
     'docs_url' => 'https://sparkison.github.io/m3u-editor-docs',
     'donate' => 'https://buymeacoffee.com/shparkison',
