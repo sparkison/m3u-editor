f96e27e6d8055f53662a91b6e479f4a1daf021ca
Shaun Parkison <shparkison@gmail.com>
2025-11-09 07:49:24 -0700

    chore: Wording update

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -10,7 +10,7 @@ class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
 
-    protected ?string $subheading = 'Manage users that can access and use the application. Each user will have their own playlists, and channels, and other resources. Some features may be restricted based on user roles (such as global settings).';
+    protected ?string $subheading = 'Manage users that can access and use the application. Each user will have their own playlists, channels, series, and other resources. Some features may be restricted based on user roles (such as global settings).';
 
     protected function getHeaderActions(): array
     {
