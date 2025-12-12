0152d08bb0b53b85ef41f4640e2123e86e0fcf5d
Shaun Parkison <shparkison@gmail.com>
2025-12-12 09:37:43 -0700

    chore: Fix sync log pagination and table actions

--- app/Filament/Resources/Playlists/Resources/PlaylistSyncStatuses/Pages/ListPlaylistSyncStatuses.php
+++ app/Filament/Resources/Playlists/Resources/PlaylistSyncStatuses/Pages/ListPlaylistSyncStatuses.php
@@ -3,19 +3,12 @@
 namespace App\Filament\Resources\Playlists\Resources\PlaylistSyncStatuses\Pages;
 
 use App\Filament\Resources\Playlists\Resources\PlaylistSyncStatuses\PlaylistSyncStatusResource;
-use App\Models\Playlist;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPlaylistSyncStatuses extends ListRecords
 {
     protected static string $resource = PlaylistSyncStatusResource::class;
-
-    public function getParent(): Playlist
-    {
-        // For nested resources, the parent ID is available in the route parameters
-        return Playlist::findOrFail(request()->route('playlist'));
-    }
 
     protected function getHeaderActions(): array
     {
@@ -25,7 +18,7 @@ class ListPlaylistSyncStatuses extends ListRecords
                 ->icon('heroicon-o-arrow-left')
                 ->color('gray')
                 ->url(function (): string {
-                    return "/playlists/{$this->getParent()->id}";
+                    return "/playlists/{$this->getParentRecord()->id}";
                 }),
             // Sync statuses are typically created automatically by the system
             // Actions\CreateAction::make(),
@@ -34,7 +27,7 @@ class ListPlaylistSyncStatuses extends ListRecords
 
     public function getTitle(): string
     {
-        $playlist = $this->getParent();
+        $playlist = $this->getParentRecord();
         return "Sync Logs for {$playlist->name}";
     }
 
--- app/Filament/Resources/Playlists/Resources/PlaylistSyncStatuses/Pages/ViewPlaylistSyncStatus.php
+++ app/Filament/Resources/Playlists/Resources/PlaylistSyncStatuses/Pages/ViewPlaylistSyncStatus.php
@@ -3,19 +3,12 @@
 namespace App\Filament\Resources\Playlists\Resources\PlaylistSyncStatuses\Pages;
 
 use App\Filament\Resources\Playlists\Resources\PlaylistSyncStatuses\PlaylistSyncStatusResource;
-use App\Models\Playlist;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewPlaylistSyncStatus extends ViewRecord
 {
     protected static string $resource = PlaylistSyncStatusResource::class;
-
-    public function getParent(): Playlist
-    {
-        // For nested resources, the parent ID is available in the route parameters
-        return Playlist::findOrFail(request()->route('playlist'));
-    }
 
     protected function getHeaderActions(): array
     {
@@ -25,7 +18,7 @@ class ViewPlaylistSyncStatus extends ViewRecord
                 ->icon('heroicon-o-arrow-left')
                 ->color('gray')
                 ->url(function (): string {
-                    return "/playlists/{$this->getParent()->id}/playlist-sync-statuses";
+                    return "/playlists/{$this->getParentRecord()->id}/playlist-sync-statuses";
                 }),
         ];
     }
