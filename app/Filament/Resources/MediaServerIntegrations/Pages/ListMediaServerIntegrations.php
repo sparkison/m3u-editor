3bbd839e18011646aff59d1589659046ef7f8ede
Shaun Parkison <shparkison@gmail.com>
2026-01-14 13:45:38 -0700

    chore: Add some extra details to the media server integration

--- app/Filament/Resources/MediaServerIntegrations/Pages/ListMediaServerIntegrations.php
+++ app/Filament/Resources/MediaServerIntegrations/Pages/ListMediaServerIntegrations.php
@@ -10,6 +10,8 @@ class ListMediaServerIntegrations extends ListRecords
 {
     protected static string $resource = MediaServerIntegrationResource::class;
 
+    protected ?string $subheading = 'Access your media server content directly within M3U Editor by integrating with popular media servers like Emby and Jellyfin. An associated playlist will be automatically created for each integration to manage content like you would for any other playlist.';
+
     protected function getHeaderActions(): array
     {
         return [
