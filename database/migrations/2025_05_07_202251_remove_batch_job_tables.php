917230a9f05ebc6195ec2d7e15539970646f6288
Shaun Parkison <shparkison@gmail.com>
2026-01-15 13:09:25 -0700

    fix: Incorrect import

--- database/migrations/2025_05_07_202251_remove_batch_job_tables.php
+++ database/migrations/2025_05_07_202251_remove_batch_job_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Filament\Schemas\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
