9b8ae79ba2aaaa593665b5ac0639df05801860cf
Shaun Parkison <shparkison@gmail.com>
2026-01-20 07:39:13 -0700

    chore: Add a small description to networks page

--- app/Filament/Resources/Networks/Pages/ListNetworks.php
+++ app/Filament/Resources/Networks/Pages/ListNetworks.php
@@ -10,6 +10,8 @@ class ListNetworks extends ListRecords
 {
     protected static string $resource = NetworkResource::class;
 
+    protected ?string $subheading = 'Networks are pseudo-TV channels built from media server content (Jellyfin/Emby/etc.) You can create M3U playlists and stream them to your IPTV apps and devices. A media server is required to use networks.';
+
     protected function getHeaderActions(): array
     {
         return [
