077c9b4437b4dc0b7f2cc4d026e4d7d122534c7c
Shaun Parkison <shparkison@gmail.com>
2026-01-20 07:32:18 -0700

    chore: Move integrations nav higher up the nav order

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -96,6 +96,8 @@ class AdminPanelProvider extends PanelProvider
             ->navigationGroups([
                 NavigationGroup::make('Playlist')
                     ->icon('heroicon-m-play-pause'),
+                NavigationGroup::make('Integrations')
+                    ->icon('heroicon-m-server-stack'),
                 NavigationGroup::make('Live Channels')
                     ->icon('heroicon-m-tv'),
                 NavigationGroup::make('VOD Channels')
@@ -104,8 +106,6 @@ class AdminPanelProvider extends PanelProvider
                     ->icon('heroicon-m-play'),
                 NavigationGroup::make('EPG')
                     ->icon('heroicon-m-calendar-days'),
-                NavigationGroup::make('Integrations')
-                    ->icon('heroicon-m-server-stack'),
                 NavigationGroup::make('Proxy')
                     ->icon('heroicon-m-arrows-right-left'),
                 NavigationGroup::make('Tools')
