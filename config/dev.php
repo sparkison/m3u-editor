42ca389ab3f157e33fb102d2dc02e83c06b7f4f0
Shaun Parkison <shparkison@gmail.com>
2026-02-09 10:56:38 -0700

    chore: Version bump to rebuild proxy

--- config/dev.php
+++ config/dev.php
@@ -3,8 +3,8 @@
 return [
     'author' => 'Shaun Parkison',
     'version' => '0.8.34',
-    'dev_version' => '0.8.35-dev',
-    'experimental_version' => '0.8.36-exp',
+    'dev_version' => '0.8.36-dev',
+    'experimental_version' => '0.9.1-exp',
     'repo' => 'm3ue/m3u-editor',
     'docs_url' => 'https://m3ue.sparkison.dev',
     'donate' => 'https://buymeacoffee.com/shparkison',
