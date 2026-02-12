a9b6de64451834066853dc5212ba18aaa4ad7a55
Shaun Parkison <shparkison@gmail.com>
2026-02-12 08:09:46 -0700

    chore: Change column type

--- database/migrations/2026_02_11_161020_add_permissions_to_users_table.php
+++ database/migrations/2026_02_11_161020_add_permissions_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('permissions')->nullable()->after('password');
+            $table->jsonb('permissions')->nullable()->after('password');
         });
     }
 
