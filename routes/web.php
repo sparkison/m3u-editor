0030ce913c7daf8046a3264d6600a150a2852efb
Shaun Parkison <shparkison@gmail.com>
2026-02-17 11:41:45 -0700

    chore: Move logic from route to controller for popout player

--- routes/web.php
+++ routes/web.php
@@ -187,38 +187,8 @@ Route::get('/shared/stream/{encodedId}.{format?}', function (string $encodedId) 
     return redirect()->route('m3u-proxy.channel', ['id' => $id]);
 })->name('shared.stream.channel');
 
-Route::get('/player/popout', function (Request $request) {
-    $streamUrl = (string) $request->query('url', '');
-
-    $hasAllowedAbsoluteScheme = filter_var($streamUrl, FILTER_VALIDATE_URL)
-        && in_array(parse_url($streamUrl, PHP_URL_SCHEME), ['http', 'https'], true);
-    $hasAllowedRelativePath = str_starts_with($streamUrl, '/');
-
-    if ($streamUrl === '' || (! $hasAllowedAbsoluteScheme && ! $hasAllowedRelativePath)) {
-        abort(404);
-    }
-
-    $streamFormat = (string) $request->query('format', 'ts');
-    if (! in_array($streamFormat, ['ts', 'mpegts', 'hls', 'm3u8'], true)) {
-        $streamFormat = 'ts';
-    }
-
-    $channelLogo = (string) $request->query('logo', '');
-    $logoHasAllowedAbsoluteScheme = filter_var($channelLogo, FILTER_VALIDATE_URL)
-        && in_array(parse_url($channelLogo, PHP_URL_SCHEME), ['http', 'https'], true);
-    $logoHasAllowedRelativePath = str_starts_with($channelLogo, '/');
-
-    if ($channelLogo !== '' && ! $logoHasAllowedAbsoluteScheme && ! $logoHasAllowedRelativePath) {
-        $channelLogo = '';
-    }
-
-    return view('player.popout', [
-        'streamUrl' => $streamUrl,
-        'streamFormat' => $streamFormat,
-        'channelTitle' => (string) $request->query('title', 'Channel Player'),
-        'channelLogo' => $channelLogo,
-    ]);
-})->name('player.popout');
+Route::get('/player/popout', [\App\Http\Controllers\PlayerController::class, 'popout'])
+    ->name('player.popout');
 
 /*
  * API routes
