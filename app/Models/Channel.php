aad0cd445c1d213f2a29863e57a6d3e982ccf274
Shaun Parkison <shparkison@gmail.com>
2026-02-17 14:50:07 -0700

    fix: Failing test cases

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -51,6 +51,7 @@ class Channel extends Model
         'last_metadata_fetch' => 'datetime',
         'epg_map_enabled' => 'boolean',
         'logo_type' => ChannelLogoType::class,
+        'sort' => 'decimal:4',
     ];
 
     public function user(): BelongsTo
