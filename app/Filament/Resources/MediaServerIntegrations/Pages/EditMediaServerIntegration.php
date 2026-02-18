502c04e331b950b7d2919e1591868cf787de7c1b
Shaun Parkison <shparkison@gmail.com>
2026-02-18 08:43:01 -0700

    chore: Update playlist view link to use view instead of edit

--- app/Filament/Resources/MediaServerIntegrations/Pages/EditMediaServerIntegration.php
+++ app/Filament/Resources/MediaServerIntegrations/Pages/EditMediaServerIntegration.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\MediaServerIntegrations\Pages;
 
 use App\Filament\Resources\MediaServerIntegrations\MediaServerIntegrationResource;
+use App\Filament\Resources\Playlists\PlaylistResource;
 use App\Jobs\SyncMediaServer;
 use App\Models\MediaServerIntegration;
 use App\Services\MediaServerService;
@@ -62,9 +63,9 @@ class EditMediaServerIntegration extends EditRecord
 
                 Action::make('viewPlaylist')
                     ->label('View Playlist')
-                    ->icon('heroicon-o-queue-list')
+                    ->icon('heroicon-o-eye')
                     ->url(fn () => $this->record->playlist_id
-                        ? route('filament.admin.resources.playlists.edit', $this->record->playlist_id)
+                        ? PlaylistResource::getUrl('view', ['record' => $this->record->playlist_id])
                         : null
                     )
                     ->visible(fn () => $this->record->playlist_id !== null),
