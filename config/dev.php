5552efc85cbb155ae47d2d70e26fe08db0ab83eb
Shaun Parkison <shparkison@gmail.com>
2026-02-20 08:30:27 -0700

    fix: Tracking logic for failed playlists
    Simplify the cache store/check/flush logic to use tags for grouping

--- config/dev.php
+++ config/dev.php
@@ -3,8 +3,8 @@
 return [
     'author' => 'Shaun Parkison',
     'version' => '0.8.39',
-    'dev_version' => '0.9.4-dev',
-    'experimental_version' => '0.9.4-exp',
+    'dev_version' => '0.9.5-dev',
+    'experimental_version' => '0.9.5-exp',
     'repo' => 'm3ue/m3u-editor',
     'docs_url' => 'https://m3ue.sparkison.dev',
     'donate' => 'https://buymeacoffee.com/shparkison',
